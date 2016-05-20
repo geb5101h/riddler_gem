73ea9b04ae8cd7b9ab0ff8ec645ea13a679dd7e4
Eric Janofsky <geb5101h@users.noreply.github.com>
2016-05-20 16:15:15 -0500

    Update gem.r

--- gem.r
+++ gem.r
@@ -3,8 +3,7 @@ a = 1 / 2
 b = 1 / 3
 c = 1 / 6
 
-
-Ea = (1 + a
+Ea = (1 + 
       + b * (1 / c - 1) * a / (a + b)
       + c * (1 / b - 1) * a / (a + c)) / (1 - a)
 
@@ -14,7 +13,7 @@ Ec = (b * 1  + a * (1 + (1 / b - 1) * a / (a + c))) / (1 - c)
 a * Ea + b * Eb + c * Ec
 
 gemCountSim <- function() {
-  vecCount = c(1,0,0)
+  vecCount = c(0,0,0)
   while (TRUE) {
     draw = rmultinom(1,1,c(a,b,c)) %>% as.vector
     vecCount = vecCount + draw
